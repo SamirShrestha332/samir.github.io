9f7c7864bd04369647e020802e04d2faac9ce5c1
SamirShrestha332 <np03cs4a220138@heraldcollege.edu.np>
2023-04-24 21:43:11 +0545

    changes

---
+++ script.js
@@ -0,0 +1,10 @@
+function myfunction(){
+    document.querySelector('.div-center').classList.add('div2');
+   
+}
+function remove(){
+    document.querySelector('.div-center').classList.remove('div2');
+    document.querySelector('.div-center').style.transition='0.5s';
+}
+let text=document.getElementById('userName');
+let pass=document.getElementById('password');
